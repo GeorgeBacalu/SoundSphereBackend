aafd794efd6c1b95c3538c0e298d58fbecbe10eb
GeorgeBacalu <georgebacalu83@gmail.com>
2024-03-12 18:00:30 +0200

    Configure database connection

--- SoundSphere.Api/Program.cs
+++ SoundSphere.Api/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using SoundSphere.Database.Context;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<SoundSphereContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sqlOptions => sqlOptions.MigrationsAssembly("SoundSphere.Api")));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
